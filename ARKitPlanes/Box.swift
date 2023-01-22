0ef1ea374cd3ea782f5fbfa09c23d8349ffc7e8a
AleksandrKotliar <sashagsmnew@gmail.com>
2023-01-22 16:20:12 +0400

    Placed objects on planes

---
+++ ARKitPlanes/Box.swift
@@ -0,0 +1,27 @@
+//
+//  Box.swift
+//  ARKitPlanes
+//
+//  Created by macbook on 22.01.23.
+//
+
+import SceneKit
+import ARKit
+
+class Box: SCNNode {
+    init(atPosition position: SCNVector3) {
+        super.init()
+        
+        let boxGeometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.green
+        
+        boxGeometry.materials = [material]
+        self.geometry = boxGeometry
+        self.position = position
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
